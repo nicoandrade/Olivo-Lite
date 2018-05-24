2f136ff50f85185f5e43e547225be7068b9d63d5
Nico Andrade <nicoandrade@gmail.com>
2018-05-24 10:49:30 -0300

    1.0.6
    - Added: Tags in portfolio items

--- template-parts/single-portfolio.php
+++ template-parts/single-portfolio.php
@@ -20,19 +20,27 @@
 
                 <div class="clearfix"></div>
 
-                <?php if( has_term( '', get_post_type() . '_category', $post->ID ) ): ?>
+                <?php if( has_term( '', get_post_type() . '_category', $post->ID ) || has_term( '', get_post_type() . '_tag', $post->ID ) ): ?>
                 <footer class="entry-footer">
         			<div class="metadata">
                         <ul>
+                            <?php if( has_term( '', get_post_type() . '_category', $post->ID ) ){ ?>
                             <li class="meta_categories">
                                 <?php esc_html_e( 'Category:', 'olivo-lite' );?>
                                 <?php echo get_the_term_list( $post->ID, get_post_type() . '_category', '', ' ' ); ?>
                             </li>
+                            <?php } ?>
+                            <?php if( has_term( '', get_post_type() . '_tag', $post->ID ) ){ ?>
+                            <li class="meta_tags">
+                                <?php esc_html_e( 'Tags:', 'olivo-lite' );?>
+                                <?php echo get_the_term_list( $post->ID, get_post_type() . '_tag', '', ', ' ); ?>
+                            </li>
+                            <?php } ?>
                         </ul>
         	            <div class="clearfix"></div>
         	        </div><!-- /metadata -->
         	    </footer><!-- .entry-footer -->
-            <?php endif; ?>
+                <?php endif; ?>
 
             </div><!-- /post_content -->
 
